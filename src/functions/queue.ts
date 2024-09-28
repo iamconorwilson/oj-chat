bc47edcfcc909333b21f67b1f1bdc893333576cf
Conor Wilson <iamconorwilson@gmail.com>
2024-09-28 07:50:15 +0100

    V1.1.0 Update
    * Update to Typescript
    * Added message history when page loaded
    * Minor tweaks to styling

--- src/functions/queue.ts
+++ src/functions/queue.ts
@@ -1,5 +1,7 @@
 const queue: QueueItem[] = [];
 
+const history: QueueItem[] = [];
+
 const enqueue = (target: string, message: any) => {
 
     const item = {
@@ -11,12 +13,55 @@ const enqueue = (target: string, message: any) => {
 }
 
 const dequeue = () => {
-    return queue.shift();
+    const item = queue.shift();
+    if (!item) return null;
+
+    addHistory(item);
+    return item;
 }
 
 const size = () => {
     return queue.length;
 }
 
+const getHistory = () => {
+    return history;
+}
 
-export { enqueue, dequeue, size }
+const addHistory = (item: QueueItem): void => {
+
+    if (item.target === 'removeSingleMessage') {
+        const index = history.findIndex(i => i.message.id === item.message.id);
+        if (index > -1) {
+            history.splice(index, 1);
+        }
+        return;
+    }
+
+    if (item.target === 'removeUserMessages') {
+        const indexes = history.filter(i => i.message.userId === item.message.user.userId);
+        indexes.forEach(i => {
+            const index = history.indexOf(i);
+            if (index > -1) {
+                history.splice(index, 1);
+            }
+        });
+        return;
+    }
+
+    if (item.target === 'removeAllMessages') {
+        history.splice(0, history.length);
+        return;
+    }
+
+    history.push(item);
+
+    if (history.length > 20) {
+        history.shift();
+    }
+    
+    return;
+}
+
+
+export { enqueue, dequeue, size, getHistory }
