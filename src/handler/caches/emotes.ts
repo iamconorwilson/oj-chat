112a68228b7d8714ed9006f887d36d2328104d08
Conor Wilson <iamconorwilson@gmail.com>
2026-08-08 23:15:09 +0100

    Update emotes.ts

--- src/handler/caches/emotes.ts
+++ src/handler/caches/emotes.ts
@@ -82,8 +82,9 @@ export class EmoteCache extends EventEmitter {
             return [];
         }
         const data = await response.json();
+        console.log(data);
         this.emoteSetId = data.emote_sets[0].id;
-        return data.emote_set.emotes.map((emote: { id: string, name: string }) => ({
+        return data.emote_sets[0].emotes.map((emote: { id: string, name: string }) => ({
             id: emote.id,
             name: emote.name,
             source: '7TV'
